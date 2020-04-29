e893added5d09f631addda3bec6b15afd389233a
Hiromitsu Matsuura <urax@wd6.so-net.ne.jp>
2020-04-29 19:14:02 +0900

    分割コンパイルをサポートしました。
    テストコードをアップデートしました。

---
+++ container.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "arccc.h"
+
+Node *new_node(NodeKind kind) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  return node;
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = new_node(kind);
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_num(int val) {
+  Node *node = new_node(ND_NUM);
+  node->val = val;
+  return node;
+}
--- test/test2.c
+++
@@ -1,8 +0,0 @@
-int plus(int x, int y) {
-  return x + y;
-}
-
-int main() {
-  return plus(3, 4);
-}
-
