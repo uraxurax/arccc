a794add02f3013b72914b43760d8135908aa2e4b
Hiromitsu Matsuura <urax@wd6.so-net.ne.jp>
2020-05-02 00:12:26 +0900

    using real stack size

--- arccc.c
+++ arccc.c
@@ -1,19 +1,18 @@
+#include "arccc.h"
+
 #include <ctype.h>
 #include <stdarg.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 
-#include "arccc.h"
-
-Token *token;
+Token* token;
 // 入力プログラム
-char *user_input;
+char* user_input;
 
 // エラーを報告するための関数
 // printfと同じ引数を取る
-void error(char *fmt, ...)
-{
+void error(char* fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
   vfprintf(stderr, fmt, ap);
@@ -22,24 +21,29 @@ void error(char *fmt, ...)
 }
 
 // エラー箇所を報告する
-void error_at(char *loc, char *fmt, ...)
-{
+void error_at(char* loc, char* fmt, ...) {
   va_list ap;
   va_start(ap, fmt);
 
   int pos = loc - user_input;
   fprintf(stderr, "%s\n", user_input);
-  fprintf(stderr, "%*s", pos, ""); // pos個の空白を出力
+  fprintf(stderr, "%*s", pos, "");  // pos個の空白を出力
   fprintf(stderr, "^ ");
   vfprintf(stderr, fmt, ap);
   fprintf(stderr, "\n");
   exit(1);
 }
 
-int main(int argc, char **argv)
-{
-  if (argc != 2)
-  {
+int get_lvar_num() {
+  int lvar_num = 0;
+  for (LVar* var = locals; var; var = var->next) {
+    lvar_num++;
+  }
+  return lvar_num;
+}
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
     error("引数の個数が正しくありません");
     return 1;
   }
@@ -59,10 +63,10 @@ int main(int argc, char **argv)
   // 変数26個分の領域を確保する
   printf("  push rbp\n");
   printf("  mov rbp, rsp\n");
-  printf("  sub rsp, 208\n");
+  printf("  sub rsp, %d\n", get_lvar_num() * 8);
 
   // 抽象構文木を下りながらコード生成
-    // 先頭の式から順にコード生成
+  // 先頭の式から順にコード生成
   for (int i = 0; code[i]; i++) {
     gen(code[i]);
 
@@ -70,7 +74,7 @@ int main(int argc, char **argv)
     // はずなので、スタックが溢れないようにポップしておく
     printf("  pop rax\n");
   }
-  
+
   // エピローグ
   // 最後の式の結果がRAXに残っているのでそれが返り値になる
   printf("  mov rsp, rbp\n");
