df9f611d6ca06b41a99e69b39cbc885fee720629
Hiromitsu Matsuura <urax@wd6.so-net.ne.jp>
2020-05-02 00:21:51 +0900

    Supporting multiple variable

--- container.c
+++ container.c
@@ -1,12 +1,13 @@
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include <ctype.h>
+
 #include "arccc.h"
 
 // 新しいトークンを作成してcurに繋げる
-Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
-  Token *tok = calloc(1, sizeof(Token));
+Token* new_token(TokenKind kind, Token* cur, char* str, int len) {
+  Token* tok = calloc(1, sizeof(Token));
   tok->kind = kind;
   tok->str = str;
   tok->len = len;
@@ -14,17 +15,29 @@ Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
   return tok;
 }
 
-bool startswith(char *p, char *q) {
+bool startswith(char* p, char* q) {
   return memcmp(p, q, strlen(q)) == 0;
 }
 
+int get_ident_len(char* p) {
+  int len = 0;
+  if (isalpha(*p) || *p == '_') {
+    len = 1;
+    p++;
+    while (isalnum(*p) || *p == '_') {
+      len++;
+      p++;
+    }
+  }
+  return len;
+}
 
 // 入力文字列pをトークナイズしてそれを返す
-Token *tokenize(void) {
+Token* tokenize(void) {
   Token head;
   head.next = NULL;
-  Token *cur = &head;
-  char *p = user_input;
+  Token* cur = &head;
+  char* p = user_input;
 
   while (*p) {
     // 空白文字をスキップ
@@ -32,25 +45,31 @@ Token *tokenize(void) {
       p++;
       continue;
     }
-    if (startswith(p, "==") || startswith(p, "!=") || 
-        startswith(p, "<=") || startswith(p, ">=") ) {
-
+    if (startswith(p, "==") || startswith(p, "!=") || startswith(p, "<=") ||
+        startswith(p, ">=")) {
       cur = new_token(TK_RESERVED, cur, p, 2);
       p += 2;
       continue;
     }
-    if (strchr("+-*/()<>=;" ,*p)) {
+    if (strchr("+-*/()<>=;", *p)) {
       cur = new_token(TK_RESERVED, cur, p++, 1);
       continue;
     }
+#if 0
     if ('a' <= *p && *p <= 'z') {
       cur = new_token(TK_IDENT, cur, p++, 1);
+    }
+#else
+    if (get_ident_len(p)) {
+      int len = get_ident_len(p);
+      cur = new_token(TK_IDENT, cur, p, len);
+      p += len;
       continue;
     }
-
+#endif
     if (isdigit(*p)) {
       cur = new_token(TK_NUM, cur, p, 0);
-      char *q = p;
+      char* q = p;
       cur->val = strtol(p, &p, 10);
       cur->len = p - q;
       continue;
--- test.sh
+++ test.sh
@@ -18,8 +18,8 @@ assert() {
 
 assert 0 '0;'
 assert 42 '42;'
-assert 21 "5+20-4;"
-assert 41 " 12 + 34 - 5 ;"
+assert 21 '5+20-4;'
+assert 41 ' 12 + 34 - 5 ;'
 assert 47 '5+6*7;'
 assert 15 '5*(9-6);'
 assert 4 '(3+5)/2;'
@@ -48,7 +48,9 @@ assert 0 '1>=2;'
 
 
 assert 3 'a=3;'
-assert 3 "a=5;b=3;"
-assert 3 "a=5;b=3;"
+assert 5 'a = 2 + 3;'
+assert 3 'a=5;b=3;'
+assert 1 'foo = 1;'
+assert 5 'bar = 2 + 3;'
 
 echo OK
